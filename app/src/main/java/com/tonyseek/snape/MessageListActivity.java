a9969b35e0bdb532a30a1bd561e3b847876b29a4
Jiangge Zhang <tonyseek@gmail.com>
2014-07-23 14:31:44 +0800

    initial commit.

---
+++ app/src/main/java/com/tonyseek/snape/MessageListActivity.java
@@ -0,0 +1,33 @@
+package com.tonyseek.snape;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class MessageListActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_message_list);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.message_list, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()) {
+            case R.id.action_settings:
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
