e5af74efad59eba26b26e72e2922f64557b10884
Jiangge Zhang <tonyseek@gmail.com>
2014-07-23 21:35:08 +0800

    move the main activity into "ui" package.

--- app/src/main/java/com/tonyseek/snape/ui/MessageListActivity.java
+++ app/src/main/java/com/tonyseek/snape/ui/MessageListActivity.java
@@ -1,9 +1,11 @@
-package com.tonyseek.snape;
+package com.tonyseek.snape.ui;
 
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.tonyseek.snape.R;
 
 
 public class MessageListActivity extends Activity {
