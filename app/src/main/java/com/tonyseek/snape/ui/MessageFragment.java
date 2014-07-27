6b95ad01daab526e35b87b7d9ee4836287570571
Jiangge Zhang <tonyseek@gmail.com>
2014-07-27 22:34:39 +0800

    implement the sms dialog with model data.

--- app/src/main/java/com/tonyseek/snape/ui/MessageFragment.java
+++ app/src/main/java/com/tonyseek/snape/ui/MessageFragment.java
@@ -1,20 +1,44 @@
 package com.tonyseek.snape.ui;
 
 import android.app.DialogFragment;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.text.format.DateUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.tonyseek.snape.R;
+import com.tonyseek.snape.gateway.ContactGateway;
+import com.tonyseek.snape.model.ContactData;
 import com.tonyseek.snape.model.SmsMessage;
 
 import butterknife.ButterKnife;
+import butterknife.InjectView;
 
 public class MessageFragment extends DialogFragment {
     private final static String EXTRA_SMS_MESSAGE = "EXTRA_SMS_MESSAGE";
 
+    private ContactGateway mContactGateway;
     private SmsMessage mSmsMessage;
+    private ContactData mContactData;
+
+    @InjectView(R.id.message_avatar)
+    ImageView mPhotoView;
+
+    @InjectView(R.id.message_person)
+    TextView mPersonView;
+
+    @InjectView(R.id.message_address)
+    TextView mAddressView;
+
+    @InjectView(R.id.message_text)
+    TextView mTextView;
+
+    @InjectView(R.id.message_date)
+    TextView mDateView;
 
     public static MessageFragment getInstance(SmsMessage smsMessage) {
         Bundle args = new Bundle();
@@ -30,14 +54,42 @@ public class MessageFragment extends DialogFragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        mContactGateway = new ContactGateway(getActivity());
+
         Bundle args = getArguments();
         mSmsMessage = args.getParcelable(EXTRA_SMS_MESSAGE);
+        mContactData = mContactGateway.getRawContact(mSmsMessage.getPersonId());
+
+        setStyle(DialogFragment.STYLE_NO_TITLE, android.R.style.Theme_Holo_Light_Dialog);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_message, container, false);
         ButterKnife.inject(this, view);
+        bindView();
         return view;
+    }
+
+    protected void bindView() {
+        Bitmap photo = mContactData.getPhoto();
+        String displayDate = DateUtils.formatDateTime(
+                getActivity(), mSmsMessage.getDate().getTime(), DateUtils.FORMAT_ABBREV_ALL);
+
+        if (mSmsMessage.getPersonId() == 0) {
+            mPersonView.setText(R.string.contact_unknown_address);
+        } else {
+            mPersonView.setText(mContactData.getDisplayName());
+        }
+
+        if (photo == null) {
+            mPhotoView.setImageResource(R.drawable.ic_contact_picture);
+        } else {
+            mPhotoView.setImageBitmap(photo);
+        }
+
+        mAddressView.setText(mSmsMessage.getAddress());
+        mTextView.setText(mSmsMessage.getTextBody());
+        mDateView.setText(displayDate);
     }
 }
